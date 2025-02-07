95c04c140a0c8e033bcbd3ff7756ca180002dc06
Joao Pedro <joao.gomes4@estudante.ifto.edu.br>
2025-02-07 08:18:20 -0300

    Inicialização do repositório bear

---
+++ src/main/java/com/example/gerenciamento_cadastro_alunos/GerenciamentoCadastroAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_cadastro_alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCadastroAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCadastroAlunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciamento_cadastro_alunos/GerenciamentoCadastroAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_cadastro_alunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoCadastroAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
